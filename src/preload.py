a8d53153cffa8b265df6b7dfec9bc9883f711ae5
Diogo Vianna V. Araújo <diogovaraujo@gmail.com>
2025-04-03 11:30:31 -0300

    src folder

--- src/preload.py
+++ src/preload.py
@@ -9,14 +9,14 @@ from qwen_vl_utils import process_vision_info
 print('Ended loading deps')
 
 model = Qwen2_5_VLForConditionalGeneration.from_pretrained(
-    "Qwen/Qwen2.5-VL-7B-Instruct",
+    "Qwen/Qwen2.5-VL-3B-Instruct",
     torch_dtype=torch.bfloat16,
     attn_implementation="flash_attention_2",
     device_map="auto",
 )
 min_pixels = 256*28*28
 max_pixels = 1280*28*28
-processor = AutoProcessor.from_pretrained("Qwen/Qwen2.5-VL-7B-Instruct", min_pixels=min_pixels, max_pixels=max_pixels)
+processor = AutoProcessor.from_pretrained("Qwen/Qwen2.5-VL-3B-Instruct", min_pixels=min_pixels, max_pixels=max_pixels)
 
 
 print('Ended loading model')
