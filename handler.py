7050f02dddf945113e405ddea45b4ebb893a2407
Diogo Vianna V. Araújo <diogoviannaaraujo@vogoiddesk.local>
2025-04-03 01:27:40 -0300

    smaller model for sanity

--- handler.py
+++ handler.py
@@ -5,7 +5,7 @@ from transformers import Qwen2_5_VLForConditionalGeneration, AutoTokenizer, Auto
 from qwen_vl_utils import process_vision_info
 
 model = Qwen2_5_VLForConditionalGeneration.from_pretrained(
-    "Qwen/Qwen2.5-VL-72B-Instruct", torch_dtype="auto", device_map="auto"
+    "Qwen/Qwen2.5-VL-7B-Instruct", torch_dtype="auto", device_map="auto"
 )
 
 processor = AutoProcessor.from_pretrained("Qwen/Qwen2-VL-7B-Instruct")
