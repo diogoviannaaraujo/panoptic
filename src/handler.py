33a145cd21805cf7c2906cb5722184d7338595af
Diogo Vianna V. Araújo <diogovaraujo@gmail.com>
2025-04-03 20:34:40 -0300

    7b maybe

--- src/handler.py
+++ src/handler.py
@@ -10,14 +10,14 @@ import torch
 print('!! Ended loading deps')
 
 model = Qwen2_5_VLForConditionalGeneration.from_pretrained(
-    "Qwen/Qwen2.5-VL-3B-Instruct",
+    "Qwen/Qwen2.5-VL-7B-Instruct",
     torch_dtype=torch.bfloat16,
     attn_implementation="flash_attention_2",
     device_map="auto",
 )
 min_pixels = 256*28*28
 max_pixels = 1280*28*28
-processor = AutoProcessor.from_pretrained("Qwen/Qwen2.5-VL-3B-Instruct", min_pixels=min_pixels, max_pixels=max_pixels)
+processor = AutoProcessor.from_pretrained("Qwen/Qwen2.5-VL-7B-Instruct", min_pixels=min_pixels, max_pixels=max_pixels)
 
 print('!! Ended loading model')
 
--- src/preload.py
+++ src/preload.py
@@ -10,7 +10,7 @@ import torch
 print('Ended loading deps')
 
 model = Qwen2_5_VLForConditionalGeneration.from_pretrained(
-    "Qwen/Qwen2.5-VL-3B-Instruct",
+    "Qwen/Qwen2.5-VL-7B-Instruct",
     torch_dtype=torch.bfloat16,
     device_map="auto",
 )
