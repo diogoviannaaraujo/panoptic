61be390c6cb0ec236f40e883bd045daa6f966e8e
Diogo Vianna V. Araújo <diogovaraujo@gmail.com>
2025-04-03 22:56:22 -0300

    more tokens?

--- src/handler.py
+++ src/handler.py
@@ -57,7 +57,7 @@ def handler(event):
     inputs = inputs.to(model.device)
 
     print("!! Starting inference generation")
-    generated_ids = model.generate(**inputs, max_new_tokens=128)
+    generated_ids = model.generate(**inputs, max_new_tokens=512)
     generated_ids_trimmed = [
         out_ids[len(in_ids) :] for in_ids, out_ids in zip(inputs.input_ids, generated_ids)
     ]
