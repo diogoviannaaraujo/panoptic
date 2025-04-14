0cc4a7b15290f374c2445c4f3e294a07cf3598ef
Diogo Vianna V. Araujo <diogoviannaaraujo@gmail.com>
2025-04-14 18:10:35 -0300

    no preload

--- src/preload.py
+++ src/preload.py
@@ -7,12 +7,12 @@ import torch
 
 print('Ended loading deps')
 
-model = Qwen2_5_VLForConditionalGeneration.from_pretrained(
-    "Qwen/Qwen2.5-VL-32B-Instruct-AWQ",
-    torch_dtype=torch.bfloat16,
-    device_map="auto",
-)
+# model = Qwen2_5_VLForConditionalGeneration.from_pretrained(
+#     "Qwen/Qwen2.5-VL-32B-Instruct-AWQ",
+#     torch_dtype=torch.bfloat16,
+#     device_map="auto",
+# )
 
-processor = AutoProcessor.from_pretrained("Qwen/Qwen2.5-VL-32B-Instruct-AWQ")
+# processor = AutoProcessor.from_pretrained("Qwen/Qwen2.5-VL-32B-Instruct-AWQ")
 
 print('Ended loading model')
