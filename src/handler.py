f4235c75606b68c27dff6363c59cd45df2ae5072
Diogo Vianna V. Araujo <diogoviannaaraujo@gmail.com>
2025-04-14 14:23:29 -0300

    go big or go home

--- src/handler.py
+++ src/handler.py
@@ -12,12 +12,12 @@ from qwen_vl_utils import process_vision_info
 print('!! Ended loading deps')
 
 model = Qwen2_5_VLForConditionalGeneration.from_pretrained(
-    "Qwen/Qwen2.5-VL-7B-Instruct",
+    "Qwen/Qwen2.5-VL-32B-Instruct-AWQ",
     torch_dtype=torch.bfloat16,
     attn_implementation="flash_attention_2",
     device_map="auto",
 )
-processor = AutoProcessor.from_pretrained("Qwen/Qwen2.5-VL-7B-Instruct")
+processor = AutoProcessor.from_pretrained("Qwen/Qwen2.5-VL-32B-Instruct-AWQ")
 
 print('!! Ended loading model')
 
--- src/preload.py
+++ src/preload.py
@@ -2,15 +2,17 @@ import runpod
 
 print('Starting preloading...')
 
-from transformers import Qwen2_5_VLForConditionalGeneration
+from transformers import Qwen2_5_VLForConditionalGeneration, AutoProcessor
 import torch
 
 print('Ended loading deps')
 
 model = Qwen2_5_VLForConditionalGeneration.from_pretrained(
-    "Qwen/Qwen2.5-VL-7B-Instruct",
+    "Qwen/Qwen2.5-VL-32B-Instruct-AWQ",
     torch_dtype=torch.bfloat16,
     device_map="auto",
 )
+
+processor = AutoProcessor.from_pretrained("Qwen/Qwen2.5-VL-32B-Instruct-AWQ")
 
 print('Ended loading model')
